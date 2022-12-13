aa41eb6937a3435bc34ed5e705ff51bce8a80cc4
lalith-at-992512424612 <lalith889@gmail.com>
2022-12-13 16:13:41 +0530

    removed

--- src/redux/rootReducer.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit'
-import { authSlice } from './auth/authSlice'
-const rootReducer = combineReducers({
-  auth: authSlice.reducer,
-})
-
-export default rootReducer
--- src/utils/baseConfig/userUtils.ts
+++
@@ -1,7 +0,0 @@
-export const getUserFromStorage = () => {
-  const userData = localStorage.getItem('user')
-  if (userData) {
-    return JSON.parse(userData)
-  }
-  return userData
-}
